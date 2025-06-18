cbfec66df945db04c056372ea7c917bda99c5076
Dennis Khodasevich <134520951+D-Khodasevich@users.noreply.github.com>
2025-06-18 10:04:53 -0700

    Update meth01_process_data.R
    Final updates to meth01

--- code/meth01_process_data.R
+++ code/meth01_process_data.R
@@ -32,7 +32,7 @@ names(meth)
 meth$platform
 
 #' `manifest` is a data.table with information about all the probes on the chip
-meth$manifest[80036:80041]
+meth$manifest[126659:126664]
 
 #' For example, `ilmn_id` is a unique identifier for each probe for EPICv2 datasets. 
 #' `probe_id` is the legacy identifier for each probe.  
@@ -41,8 +41,7 @@ meth$manifest[80036:80041]
 #' by these probes. Here, all the probes are CpG sites, i.e., "cg".
 
 #' QUESTION: What other type of loci besides CpG sites are there?
-#' ANSWER: There are also CHH sites and SNP probes
-#' ANSWER: EPICv2 additionally has nv probes
+#' ANSWER: There are also CHH sites (ch), SNPs (rs), and new variants (nv).
 
 #' The column `channel` tells us the color channel and Infinium probe design type.
 #' "Grn" and "Red" denote Infinium Type I, "Both" denotes Type II.
@@ -81,9 +80,9 @@ dim(meth$N)
 meth$N[290:292,1:3]
 
 #' QUESTION: How many probes are missing in the first sample?
-#' ANSWER: 11 probes have no beads present for either the unmethylated or
+#' ANSWER: 16 probes have no beads present for either the unmethylated or
 #' methylated type
- 
+
 #' QUESTION: Why are for type II probes the entries in `N` and `V` are always the same.
 #' ANSWER: Because the same beads are used to measure methylated and unmethylated signal
 
@@ -107,8 +106,8 @@ pheno[,exclude:=FALSE]
 #' `control_metrics()` calculates the control metrics from the control probes
 #' `sample_failure()` evaluates control metrics against pre-defined thresholds
 
-meth %>% control_metrics %>% sample_failure -> pheno$failed
-#' Here we are making use of the pipe operator (%>%) from the `magrittr` package.
+meth |> control_metrics() |> sample_failure() -> pheno$failed
+#' Here we are making use of the pipe operator (|>) from the `magrittr` package.
 #' The line above is equivalent to (i.e., could be alternatively written as)
 # pheno$failed = sample_failure(control_metrics(meth))
 
@@ -127,18 +126,18 @@ meth = ewastools::detectionP(meth)
 #' Comparison of detection p-values for probes targeting the Y chromosome across
 #' males and females
 #' Get the indices of the probes
-chrY = meth$manifest[chr=='chrY', index] 
+chrY = meth$manifest[chr == "chrY", index] 
 #' There are 667 such probes on EPICv2
 length(chrY)
 #' Retrieve the corresponding p-values
-detP = meth$detP[chrY,]
+detP = meth$detP[chrY, ]
 #' Count those below 0.01 as detected
-detP = colSums(detP<0.01,na.rm=TRUE)
+detP = colSums(detP < 0.01, na.rm = TRUE)
 
-boxplot(split(detP,pheno$Sex),ylab="# of detected Y chromosome probes")
-split(detP,pheno$Sex) %>% sapply(median)
+boxplot(split(detP, pheno$Sex), ylab = "# of detected Y chromosome probes")
+split(detP, pheno$Sex) |> sapply(median)
 
-#' Almost all of the 667 chromosome probes are called detected in the males
+#' Almost all of the 667 Y-chromosomal probes are called detected in the males
 #' whereas among females 141 probes are detected on average.
 #'
 #' QUESTION: Excluding the Y chromosome and missing probes, what is the percentage
@@ -160,32 +159,32 @@ split(detP,pheno$Sex) %>% sapply(median)
 #' `correct_dye_bias()` adjusts the green color channel using the red color channel
 #' as reference. The adjustment is performed on the log scale.
 
-meth %>% dont_normalize                      -> with_bias
-meth %>% correct_dye_bias %>% dont_normalize -> corrected
+meth |> dont_normalize()                       -> with_bias
+meth |> correct_dye_bias() |> dont_normalize() -> corrected
 
 #' Looking at the same loci as before we can check whether dye-bias correction
 #' changed the beta values
 #' Each probe is of a different type/color channel
 meth$manifest$channel[290:292]
 
-with_bias[290:292,1:3] %>% round(4)
-corrected[290:292,1:3] %>% round(4)
+with_bias[290:292, 1:3] |> round(4)
+corrected[290:292, 1:3] |> round(4)
 
 #' QUESTION: Why are the beta values for cg07197585_BC11 unchanged?
 #' ANSWER: Because the red color channel is used as reference.
 
 #' Plotting beta values for heterozygous SNPs, we can observe the dye bias as a
 #' deviation from 0.5. For the corrected data, the middle peak aligns with 0.5
-snps = meth$manifest[probe_type=="rs" & channel=="Both"]$index
+snps = meth$manifest[probe_type == "rs" & channel == "Both"]$index
 
-plot (density(with_bias[snps,23], na.rm=TRUE, bw=0.1),col=1)
-lines(density(corrected[snps,23], na.rm=TRUE, bw=0.1),col=2)
-abline(v=0.5,lty=3)
-legend("topleft",col=1:2,legend=c("with_bias","corrected"),lwd=1)
+plot (density(with_bias[snps, 23], na.rm = TRUE, bw = 0.1), col = 1)
+lines(density(corrected[snps, 23], na.rm = TRUE, bw = 0.1), col = 2)
+abline(v = 0.5, lty = 3)
+legend("topleft", col = 1:2, legend = c("with_bias", "corrected"), lwd = 1)
 
 
 
-meth %>% correct_dye_bias -> meth
+meth |> correct_dye_bias() -> meth
 rm(corrected,with_bias)
 
 
@@ -209,11 +208,11 @@ pheno[Sex != predicted_sex, .(gsm, Sex, predicted_sex)]
 #' likely means that this sample was mislabeled.
 
 #' We flag this sample for exclusion
-pheno[Sex!=predicted_sex,exclude:=TRUE]
+pheno[Sex != predicted_sex,exclude:=TRUE]
 
-#' Let's also plot these data
-plot(Y~X, data=pheno, type="n")
-text(Y~X, labels=Sex, col=ifelse(Sex=="m", 2, 1), data=pheno)
+#' Let's also plot this data
+plot(Y ~ X, data = pheno, type = "n")
+text(Y ~ X, labels = Sex, col = ifelse(Sex == "m", 2, 1), data = pheno)
 #' The mislabeled sample can be easily spotted as it falls into the wrong cluster.
 
 
@@ -231,12 +230,12 @@ beta = dont_normalize(meth)
 #' `snp_outliers()` returns the average log odds of a SNP probe belonging to
 #' the outlier component of the mixture model. Here a cut-off of -4 is used
 #' as threshold for exclusion.
-snps = meth$manifest[probe_type=="rs"]$index
-genotypes = call_genotypes(beta[snps, ], learn=FALSE)
+snps = meth$manifest[probe_type == "rs"]$index
+genotypes = call_genotypes(beta[snps, ], learn = FALSE)
 pheno$outlier = snp_outliers(genotypes)
 
-stripchart(pheno$outlier, method="jitter", pch=4, xlim = c(-6, -3.5))
-abline(v=-4, lty="dotted", col=2)
+stripchart(pheno$outlier, method = "jitter", pch = 4, xlim = c(-6, -3.5))
+abline(v = -4, lty = "dotted", col = 2)
 
 #' None of the samples fail this QC check
 
@@ -248,8 +247,8 @@ abline(v=-4, lty="dotted", col=2)
 pheno$donor_id = enumerate_sample_donors(genotypes)
 
 #' Find donor IDs represented more than once
-pheno[,n:=.N,by=donor_id]
-pheno[n>1,.(gsm,donor_id)]
+pheno[, n := .N, by = donor_id]
+pheno[n > 1, .(gsm,donor_id)]
 
 #' None of the samples fail this QC check
 
@@ -274,15 +273,15 @@ pcs = pcs$u
 pheno$pc1 = pcs[,1]
 pheno$pc2 = pcs[,2]
 
-plot(pc2~pc1, col=ifelse(Sex=="M",2,1), data=pheno)
-text(pc2~pc1, labels=pheno[32]$gsm, data=pheno[32], pos=4,offset=1, col=2)
-text(pc2~pc1, labels=pheno[33]$gsm, data=pheno[33], pos=4, offset = -4, col=2)
+plot(pc2 ~ pc1, col = ifelse(Sex == "M", 2, 1), data = pheno)
+text(pc2 ~ pc1, labels = pheno[32]$gsm, data = pheno[32], pos = 4, offset =  1, col = 2)
+text(pc2 ~ pc1, labels = pheno[33]$gsm, data = pheno[33], pos = 4, offset = -4, col = 2)
 
 #' GSM8461228 is actually a brain tissue sample from another GEO
 #' dataset. It dominates the first principal component and should be excluded as
 #' it could drastically change the results of the downstream analysis.
 
-pheno[gsm=="GSM8461228",exclude:=TRUE]
+pheno[gsm == "GSM8461228", exclude := TRUE]
 
 #' GSM8326502 is an isolated T cell sample that should also be removed based on the PC plot,
 #' but we will temporarily leave it in to demonstrate cell proportion estimates
@@ -302,33 +301,33 @@ pheno[gsm=="GSM8461228",exclude:=TRUE]
 #' of options). The function operates on the matrix of beta values.
 
 # Option "HRS" provides proportions of 15 cell types
-colnames(estimateLC(beta, ref="HRS"))
+colnames(estimateLC(beta, ref = "HRS"))
 
 #' we are using the Reinius reference dataset
-LC = estimateLC(beta, ref="Reinius", constrained = TRUE)
+LC = estimateLC(beta, ref = "Reinius", constrained = TRUE)
 
 #' `LC` is a data.table. We can easily add to `pheno`
 pheno = cbind(pheno, LC)
 
 #' Plot the proportions of CD4 versus (the column named `CD4`)
-plot(sort(pheno$CD4), ylim=c(0,1))
+plot(sort(pheno$CD4), ylim = c(0, 1))
 
 #' We observe one outlier, a sample with a CD4 proportion >80%
 pheno[which.max(CD4), .(gsm,exclude)]
 #' GSM8326502 is an isolated T cell sample. Flag for exclusion.
-pheno[gsm=="GSM8326502", exclude:=TRUE]
+pheno[gsm == "GSM8326502", exclude := TRUE]
 
 
 
 #' Let's confirm that the leukocyte composition is confounded with smoking status
 #' LC stratified by smoking status
 LC$smoker = pheno$smoker
-LC = melt(LC,value.name="proportion",variable.name="cell_type",id.vars="smoker")
+LC = melt(LC, value.name = "proportion", variable.name = "cell_type", id.vars="smoker")
 
-boxplot(proportion ~ smoker+cell_type,LC,col=1:2,
-	main="Cell type distribution by smoking status",xaxt="n")
-axis(1,at=seq(from=1.5, to=11.5,by=2),adj=1,labels=unique(LC$cell_type))
-legend("topright",c("Non-smoker","Smoker"),pch=15,bty='n',col=1:2)
+boxplot(proportion ~ smoker + cell_type, LC, col = 1:2,
+        main="Cell type distribution by smoking status",xaxt="n")
+axis(1,at=seq(from = 1.5, to = 11.5, by = 2), adj = 1, labels = unique(LC$cell_type))
+legend("topright", c("Non-smoker", "Smoker"), pch = 15, bty = 'n', col = 1:2)
 
 #' We can see a difference in the proportions of natural killer cells between
 #' smokers and non-smokers
@@ -351,17 +350,17 @@ head(manifest[, 1:2])
 beta <- sesame::betasCollapseToPfx(beta)
 
 #' Save a subset of the beta matrix, excluding non-autosomal probes and non-cg probes
-toExclude <- subset(manifest, manifest$probe_type!="cg" | 
-                      manifest$chr %in% c("chrX","chrY","chr0","chrM"))
+toExclude <- subset(manifest, manifest$probe_type != "cg" | 
+                      manifest$chr %in% c("chrX", "chrY", "chr0", "chrM"))
 toExclude <- toExclude$probe_id
-beta <- beta[! row.names(beta) %in% toExclude, ]
+beta <- beta[!row.names(beta) %in% toExclude, ]
 
 manifest <- subset(manifest, manifest$probe_type=="cg" & 
-                      !manifest$chr %in% c("chrX","chrY","chr0","chrM"))
+                     !manifest$chr %in% c("chrX", "chrY", "chr0", "chrM"))
 
 #' Also save a copy of the manifest
 manifest <- subset(manifest, manifest$probe_id %in% row.names(beta))
-manifest <- manifest[match(unique(manifest$probe_id), manifest$probe_id),]
+manifest <- manifest[match(unique(manifest$probe_id), manifest$probe_id), ]
 
 
 #' Write data to disk. (Do not actually run the next line. Only when
